570368de406fb2a1898e3d9ba5e357340a9b4333
chebihiF <chebihi.f@ucd.ac.ma>
2023-01-26 16:28:52 +0100

    first commit

---
+++ assets/scripts/app.js
@@ -0,0 +1,41 @@
+const ATTACK_VALUE = 10;
+const STRONG_ATTACK_VALUE = 17;
+const MONSTER_ATTACK_VALUE = 14;
+const HEAL_VALUE = 20;
+
+let chosenMaxLife = 100;
+let currentMonsterHealth = chosenMaxLife;
+let currentPlayerHealth = chosenMaxLife;
+
+adjustHealthBars(chosenMaxLife);
+
+function healPlayerHandler() {
+  increasePlayerHealth(HEAL_VALUE);
+}
+
+function attackMonster(mode) {
+  let maxDamage;
+  if (mode == "ATTACK") maxDamage = ATTACK_VALUE;
+  else maxDamage = STRONG_ATTACK_VALUE;
+  const damage = dealMonsterDamage(maxDamage);
+  currentMonsterHealth -= damage;
+  const playerDamage = dealPlayerDamage(maxDamage);
+  currentPlayerHealth -= playerDamage;
+  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) alert("You win");
+  else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0)
+    alert("You Lost !!!");
+  else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0)
+    alert("You have a draw !!!");
+}
+
+function strongAttackHandler() {
+  attackMonster("ATTACK");
+}
+
+function attackHandler() {
+  attackMonster("STRONG_ATTACK");
+}
+
+healBtn.addEventListener("click", healPlayerHandler);
+strongAttackBtn.addEventListener("click", strongAttackHandler);
+attackBtn.addEventListener("click", attackHandler);
