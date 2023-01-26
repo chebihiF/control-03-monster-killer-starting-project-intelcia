12e9a9c43f7111e0475fc015e31d3a8adf000232
chebihiF <chebihi.f@ucd.ac.ma>
2023-01-26 17:18:05 +0100

    final version

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,8 +2,15 @@ const ATTACK_VALUE = 10;
 const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
 const HEAL_VALUE = 20;
+const logEntries = [];
 
-let chosenMaxLife = 100;
+let enteredValue = prompt("Maximum life for you and the monster : ", "100");
+
+if (isNaN(enteredValue) || enteredValue <= 0) {
+  enteredValue = 100;
+}
+
+let chosenMaxLife = enteredValue;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 let bonnus = true;
@@ -12,6 +19,23 @@ adjustHealthBars(chosenMaxLife);
 
 function healPlayerHandler() {
   increasePlayerHealth(HEAL_VALUE);
+}
+
+function writeToLog(ev, val, monsterHealth, playerHealth) {
+  let logEntry = {
+    event: ev,
+    value: val,
+    finalMonsterHealth: monsterHealth,
+    finalPlayerHealth: playerHealth,
+  };
+  logEntries.push(logEntry);
+}
+
+function reset() {
+  currentMonsterHealth = chosenMaxLife;
+  currentPlayerHealth = chosenMaxLife;
+  monsterHealthBar.value = chosenMaxLife;
+  playerHealthBar.value = chosenMaxLife;
 }
 
 function attackMonster(mode) {
@@ -23,18 +47,40 @@ function attackMonster(mode) {
   const playerDamage = dealPlayerDamage(maxDamage);
   currentPlayerHealth -= playerDamage;
 
+  writeToLog(mode, maxDamage, currentMonsterHealth, currentPlayerHealth);
+
   if (currentPlayerHealth <= 0 && bonnus) {
     increasePlayerHealth(HEAL_VALUE);
     currentPlayerHealth += HEAL_VALUE;
     alert("You would be dead but the bonnus save your live");
     bonnus = false;
+    removeBonusLife();
   }
 
-  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) alert("You win");
-  else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0)
+  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+    alert("You win");
+    writeToLog(
+      "PLAYER WIN",
+      maxDamage,
+      currentMonsterHealth,
+      currentPlayerHealth
+    );
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+    writeToLog(
+      "MONSTER WIN",
+      maxDamage,
+      currentMonsterHealth,
+      currentPlayerHealth
+    );
     alert("You Lost !!!");
-  else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0)
+  } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
     alert("You have a draw !!!");
+    writeToLog("DRAW", maxDamage, currentMonsterHealth, currentPlayerHealth);
+  }
+
+  if (currentMonsterHealth <= 0 || currentPlayerHealth <= 0) {
+    reset();
+  }
 }
 
 function strongAttackHandler() {
@@ -45,6 +91,11 @@ function attackHandler() {
   attackMonster("STRONG_ATTACK");
 }
 
+function printLogs() {
+  console.log(logEntries);
+}
+
 healBtn.addEventListener("click", healPlayerHandler);
 strongAttackBtn.addEventListener("click", strongAttackHandler);
 attackBtn.addEventListener("click", attackHandler);
+logBtn.addEventListener("click", printLogs);
